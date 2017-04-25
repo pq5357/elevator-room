faed7a4fa9e2b3c25f7e7aec183125ccff3c2b31
Eric Richardson <eric.richardson97@gmail.com>
2017-04-25 10:33:51 -0400

    Fixes from PR

--- app/src/main/java/com/willowtreeapps/android/elevatorroom/LobbyActivity.java
+++ app/src/main/java/com/willowtreeapps/android/elevatorroom/LobbyActivity.java
@@ -2,18 +2,12 @@ package com.willowtreeapps.android.elevatorroom;
 
 import android.arch.lifecycle.LifecycleActivity;
 import android.arch.lifecycle.ViewModelProviders;
-
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v4.view.ViewCompat;
 import android.view.View;
 import android.widget.TextView;
-
-import com.willowtreeapps.android.elevatorroom.persistence.VisitedFloor;
-
-import org.reactivestreams.Subscription;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -22,17 +16,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.disposables.Disposables;
-import io.reactivex.functions.Consumer;
-
-
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
+import io.reactivex.disposables.Disposables;
 import io.reactivex.schedulers.Schedulers;
 
 public class LobbyActivity extends LifecycleActivity {
@@ -47,7 +34,7 @@ public class LobbyActivity extends LifecycleActivity {
     View person;
     @BindView(R.id.playfield)
     View playField;
-    Disposable disposable;
+    Disposable intervalDisposable = Disposables.disposed();
 
 
     @Override
@@ -65,7 +52,7 @@ public class LobbyActivity extends LifecycleActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        disposable = Observable.interval(5, TimeUnit.SECONDS)
+        intervalDisposable = Observable.interval(5, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.computation())
                 .subscribe(aLong -> animatePerson());
@@ -74,17 +61,17 @@ public class LobbyActivity extends LifecycleActivity {
 
     private boolean startDrag() {
         person.startDragAndDrop(null, getShadow(person), Boolean.TRUE,
-                View.DRAG_FLAG_GLOBAL|View.DRAG_FLAG_GLOBAL_URI_READ|
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ |
                         View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION);
         return true;
     }
 
     private View.DragShadowBuilder getShadow(View v) {
-        return new View.DragShadowBuilder(){
+        return new View.DragShadowBuilder() {
             @Override
             public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
-                outShadowSize.set(v.getWidth()/8, v.getHeight()/8);
-                outShadowTouchPoint.set(outShadowSize.x/2, outShadowSize.y/2);
+                outShadowSize.set(v.getWidth() / 8, v.getHeight() / 8);
+                outShadowTouchPoint.set(outShadowSize.x / 2, outShadowSize.y / 2);
             }
 
             @Override
@@ -110,9 +97,7 @@ public class LobbyActivity extends LifecycleActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(disposable != null && !disposable.isDisposed()) {
-            disposable.dispose();
-        }
+        intervalDisposable.dispose();
         if (unbinder != null) {
             unbinder.unbind();
         }
