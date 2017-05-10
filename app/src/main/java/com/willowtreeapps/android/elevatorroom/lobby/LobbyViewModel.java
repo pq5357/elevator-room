5624323f6e6a85e2ec23262ad1d751dfa3343a86
sunian <jamessun92@gmail.com>
2017-05-10 11:18:52 -0400

    moved timer to LiveData removed drag drop code

--- app/src/main/java/com/willowtreeapps/android/elevatorroom/lobby/LobbyViewModel.java
+++ app/src/main/java/com/willowtreeapps/android/elevatorroom/lobby/LobbyViewModel.java
@@ -24,12 +24,14 @@ public class LobbyViewModel extends ViewModel {
 
     private final GameDatabase database;
     public final LiveData<Long> gameLoopTimer;
+    public final LiveData<Long> newPersonTimer;
     public final LiveData<VisitedFloor> currentFloor;
 
     public LobbyViewModel() {
         database = MyApplication.getGameDatabase();
         currentFloor = database.floorDao().currentFloor();
         gameLoopTimer = LiveDataRx.fromEternalPublisher(FlowableOnBackpressureDrop.interval(FRAME_LENGTH, TimeUnit.MILLISECONDS));
+        newPersonTimer = LiveDataRx.fromEternalPublisher(FlowableOnBackpressureDrop.interval(10, TimeUnit.SECONDS));
     }
 
     /**
@@ -44,11 +46,12 @@ public class LobbyViewModel extends ViewModel {
         );
     }
 
-    public void fakeNew() {
+    public void generatePerson() {
         int goal = (int) (Math.random() * ElevatorViewModel.TOTAL_FLOORS);
+        int start = (int) (goal + Math.random() * (ElevatorViewModel.TOTAL_FLOORS - 1) + 1) % ElevatorViewModel.TOTAL_FLOORS;
         final Person person = new Person(
                 System.currentTimeMillis() + DateUtils.SECOND_IN_MILLIS * 15,
-                1, 0
+                goal, start
         );
         RxUtil.runInBg(() -> database.personDao().newPerson(person));
     }
